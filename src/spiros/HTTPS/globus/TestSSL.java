29e3511e21e16d38604d2c4b520a749f6cae7f07
koulouzis <skoulouzis@users.noreply.github.com>
2010-05-12 12:13:11 +0000

    clean up

--- src/spiros/HTTPS/globus/TestSSL.java
+++ src/spiros/HTTPS/globus/TestSSL.java
@@ -1,33 +1,13 @@
 package spiros.HTTPS.globus;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
 import java.net.MalformedURLException;
-import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.UnknownHostException;
 
-import javax.net.SocketFactory;
-
-import org.globus.common.CoGProperties;
-import org.globus.gsi.GSIConstants;
-import org.globus.gsi.GlobusCredential;
-import org.globus.gsi.TrustedCertificates;
-import org.globus.gsi.gssapi.GSSConstants;
-import org.globus.gsi.gssapi.GlobusGSSCredentialImpl;
-import org.globus.gsi.gssapi.net.GssSocket;
-import org.globus.gsi.gssapi.net.GssSocketFactory;
 import org.globus.net.GSIHttpURLConnection;
-import org.gridforum.jgss.ExtendedGSSContext;
-import org.gridforum.jgss.ExtendedGSSCredential;
-import org.gridforum.jgss.ExtendedGSSManager;
-import org.ietf.jgss.GSSCredential;
-import org.ietf.jgss.GSSException;
-import org.ietf.jgss.GSSManager;
 
 public class TestSSL
 {
@@ -39,7 +19,7 @@ public class TestSSL
 
         try
         {
-             doit1();
+            doit1();
 
         }
         catch (Exception e)
@@ -61,12 +41,11 @@ public class TestSSL
             // conn = new org.apache.commons.httpclient.util.HttpURLConnection(
             // new org.apache.commons.httpclient.methods.GetMethod(), new URL(
             // "http://en.wikipedia.org/"));
-//            conn = new MYGSIHttpURLConnection(new URL("https://grasveld.nikhef.nl:9000/"));
+            // conn = new MYGSIHttpURLConnection(new
+            // URL("https://grasveld.nikhef.nl:9000/"));
             conn = new GSIHttpURLConnection(new URL("https://grasveld.nikhef.nl:9000/"));
-            conn.setRequestProperty("gssMode","ssl");
-            
-            
-            
+            conn.setRequestProperty("gssMode", "ssl");
+
             conn.connect();
 
             System.err.println("ContentType: " + conn.getContentType());
@@ -74,11 +53,8 @@ public class TestSSL
             System.err.println("ContentEncoding: " + conn.getContentEncoding());
 
             System.err.println("ContentLength: " + conn.getContentLength());
-            
-            
-            System.err.println("Content: " + conn.getContent().getClass().getName());
 
-             
+            System.err.println("Content: " + conn.getContent().getClass().getName());
 
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String inputLine;
@@ -86,8 +62,6 @@ public class TestSSL
             while ((inputLine = in.readLine()) != null)
                 System.out.println(inputLine);
             in.close();
-            
-            
 
         }
         catch (MalformedURLException e)
