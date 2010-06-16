faa608aec77e32eea1b1b27d468693157de9fd2d
koulouzis <skoulouzis@users.noreply.github.com>
2010-06-16 12:31:33 +0000

    webdav dependencies

--- src/spiros/webdav/TestWebDav.java
+++ src/spiros/webdav/TestWebDav.java
@@ -1,18 +1,18 @@
 package spiros.webdav;
 
-import java.io.IOException;
-
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpConnectionManager;
-import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.OptionsMethod;
 import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
 import org.apache.jackrabbit.webdav.client.methods.CopyMethod;
 import org.apache.jackrabbit.webdav.client.methods.DavMethod;
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
 
 public class TestWebDav
 {
@@ -20,9 +20,9 @@ public class TestWebDav
     {
 
         HostConfiguration hostConfig = new HostConfiguration();
-        
+
         hostConfig.setHost("localhost", 8008);
-        
+
         HttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
         HttpConnectionManagerParams params = new HttpConnectionManagerParams();
         int maxHostConnections = 20;
@@ -36,18 +36,25 @@ public class TestWebDav
         // source, dest, overwrite
         DavMethod copy = new CopyMethod("http://localhost:8008/vlet_devplan3.pdf",
                 "http://localhost:8008/vlet_devplan3.pdf.copy", true);
-        
+
+        GetMethod get = new GetMethod("http://localhost:8008/eclipse");
+
         try
         {
-//            client.executeMethod(copy);
+            PropFindMethod propFind = new PropFindMethod("http://localhost:8008/");
+
+            // client.executeMethod(copy);
+
+            int status = client.executeMethod(propFind);
+
+            System.out.println(propFind.getStatusCode() + " " + propFind.getStatusText());
+
         }
         catch (Exception e)
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-//        System.out.println(copy.getStatusCode() + " " + copy.getStatusText());
 
     }
 }
