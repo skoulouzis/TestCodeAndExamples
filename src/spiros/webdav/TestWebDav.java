1302909fe5f6e7c3285ebe1abebe5b27bdb7a5f7
koulouzis <skoulouzis@users.noreply.github.com>
2010-06-17 12:40:59 +0000

    new

--- src/spiros/webdav/TestWebDav.java
+++ src/spiros/webdav/TestWebDav.java
@@ -1,9 +1,5 @@
 package spiros.webdav;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.httpclient.HttpClient;
@@ -12,17 +8,21 @@ import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.OptionsMethod;
 import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
 import org.apache.jackrabbit.webdav.DavConstants;
 import org.apache.jackrabbit.webdav.MultiStatus;
 import org.apache.jackrabbit.webdav.MultiStatusResponse;
+import org.apache.jackrabbit.webdav.Status;
 import org.apache.jackrabbit.webdav.client.methods.CopyMethod;
 import org.apache.jackrabbit.webdav.client.methods.DavMethod;
 import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
+import org.apache.jackrabbit.webdav.property.DavProperty;
+import org.apache.jackrabbit.webdav.property.DavPropertyIterator;
+import org.apache.jackrabbit.webdav.property.DavPropertyName;
+import org.apache.jackrabbit.webdav.property.DavPropertyNameIterator;
+import org.apache.jackrabbit.webdav.property.DavPropertyNameSet;
 import org.apache.jackrabbit.webdav.property.DavPropertySet;
-import org.apache.jackrabbit.webdav.property.DefaultDavProperty;
-import org.apache.jackrabbit.webdav.property.PropEntry;
+import org.apache.xerces.dom.DeferredElementNSImpl;
 
 public class TestWebDav
 {
@@ -35,7 +35,7 @@ public class TestWebDav
 
         HttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
         HttpConnectionManagerParams params = new HttpConnectionManagerParams();
-        int maxHostConnections = 20;
+        int maxHostConnections = 30;
         params.setMaxConnectionsPerHost(hostConfig, maxHostConnections);
         connectionManager.setParams(params);
         HttpClient client = new HttpClient(connectionManager);
@@ -47,48 +47,89 @@ public class TestWebDav
         DavMethod copy = new CopyMethod("http://localhost:8008/vlet_devplan3.pdf",
                 "http://localhost:8008/vlet_devplan3.pdf.copy", true);
 
-        GetMethod get = new GetMethod("http://localhost:8008/eclipse");
+        GetMethod get = new GetMethod("http://localhost:8008/");
 
         try
         {
 
             // Read all properties of a resource
-            DavMethod pFind = new PropFindMethod("http://localhost:8008/HttpClient.java",
-                    DavConstants.PROPFIND_ALL_PROP, DavConstants.DEPTH_INFINITY);
-            client.executeMethod(pFind);
+//            DavMethod pFind = new PropFindMethod("http://localhost:8008/file2",
+//                    DavConstants.PROPFIND_ALL_PROP, DavConstants.DEPTH_INFINITY);
+//            client.executeMethod(pFind);
 
-            MultiStatus multiStatus = pFind.getResponseBodyAsMultiStatus();
+//            MultiStatus multiStatus = pFind.getResponseBodyAsMultiStatus();
 
-            // Not quite nice, but for a example ok
-            DavPropertySet props = multiStatus.getResponses()[0].getProperties(200);
+//            // Not quite nice, but for a example ok
+//            DavPropertySet props = multiStatus.getResponses()[0].getProperties(200);
+//
+//            Collection propertyColl = props.getContent();
+//            propertyColl.iterator();
+//            for (Iterator iterator = propertyColl.iterator(); iterator.hasNext();)
+//            {
+//                DefaultDavProperty tmpProp = (DefaultDavProperty) iterator.next();
+//                System.out.println(tmpProp.getName() + "  " + tmpProp.getValue());
+//                
+//            }
 
-            Collection propertyColl = props.getContent();
-            propertyColl.iterator();
-            for (Iterator iterator = propertyColl.iterator(); iterator.hasNext();)
-            {
-                DefaultDavProperty tmpProp = (DefaultDavProperty) iterator.next();
-                System.out.println(tmpProp.getName() + "  " + tmpProp.getValue());
-            }
-
+                        
             // Getting a list of subresources of a resource
-            pFind = new PropFindMethod("http://localhost:8008/", DavConstants.PROPFIND_ALL_PROP, DavConstants.DEPTH_1);
-            client.executeMethod(pFind);
-
-            multiStatus = pFind.getResponseBodyAsMultiStatus();
+            PropFindMethod method = new PropFindMethod("http://localhost:8008/", DavConstants.PROPFIND_ALL_PROP, DavConstants.DEPTH_0);
+            
+            
+            client.executeMethod(method);
+            
+            MultiStatus multiStatus = method.getResponseBodyAsMultiStatus();
+            
             MultiStatusResponse[] responses = multiStatus.getResponses();
             MultiStatusResponse currResponse;
-            ArrayList files = new ArrayList();
+            
             System.out.println("Folders and files in http://localhost:8008/ :");
             for (int i = 0; i < responses.length; i++)
             {
                 currResponse = responses[i];
 
-                System.out.println(currResponse.getHref());
+                System.out.println("getHref: "+currResponse.getHref());
+                System.out.println("getResponseDescription: "+currResponse.getResponseDescription());
+                
+                Status[] status = currResponse.getStatus();
+                for(int j=0;j<status.length;j++){
+                    System.out.println("    status: "+status[j].getStatusCode());
+                                       
+                    
+                    DavPropertySet props = currResponse.getProperties(status[j].getStatusCode());
+                    DavPropertyIterator propIt = props.iterator();
+                    
+                    while(propIt.hasNext()){
+                        DavProperty<?> prop = propIt.nextProperty();
+                        DavPropertyName name = prop.getName();
+                        Object value = prop.getValue();
+                        if(value instanceof org.apache.xerces.dom.DeferredElementNSImpl){
+                            org.apache.xerces.dom.DeferredElementNSImpl deferredElementNSImpl = (DeferredElementNSImpl) value;  
+                            
+                            System.out.println("getBaseURI: "+deferredElementNSImpl.getBaseURI());
+                            System.out.println("getLength: "+deferredElementNSImpl.getLength());
+                            System.out.println("getLocalName: "+deferredElementNSImpl.getLocalName());
+                            System.out.println("getNamespaceURI: "+deferredElementNSImpl.getNamespaceURI());
+                            System.out.println("getNodeIndex: "+deferredElementNSImpl.getNodeIndex());
+                            System.out.println("getNodeName: "+deferredElementNSImpl.getNodeName());
+                            System.out.println("getNodeType: "+deferredElementNSImpl.getNodeType());
+                            System.out.println("getNodeValue: "+deferredElementNSImpl.getNodeValue());
+                            System.out.println("getPrefix: "+deferredElementNSImpl.getPrefix());
+                            System.out.println("getTagName: "+deferredElementNSImpl.getTagName());
+                            System.out.println("getTextContent: "+deferredElementNSImpl.getTextContent());
+                            System.out.println("getOwnerDocument().getLocalName(): "+deferredElementNSImpl.getOwnerDocument().getLocalName());
+                            
+                            System.out.println("deferredElementNSImpl    "+name.getName()+" : "+deferredElementNSImpl.getNodeName());
+                            
+                            
+                        }else{
+                            System.out.println("    "+name.getName()+" : "+value);   
+                        }
+                        if(value !=null)
+                        System.out.println("        propNamegetValuegetClass().getName: "+value.getClass().getName());
+                    }
+                }
 
-                // if (!(currResponse.getHref().equals(path) ||
-                // currResponse.getHref().equals(path + "/"))) {
-                // System.out.println(currResponse.getHref());
-                // }
             }
 
         }
